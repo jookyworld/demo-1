e060c39bdacceba158fbdabaeff77e75e583842b
jooky <jooky0327@naver.com>
2025-10-28 11:02:13 +0900

    first commit

---
+++ src/main/java/com/back/HomeController.java
@@ -0,0 +1,18 @@
+package com.back;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return """
+                <h1>Welcome to the Home Page!</h1>
+                                <p>This is a Demo Spring Boot application.</p>
+                """;
+    }
+}
