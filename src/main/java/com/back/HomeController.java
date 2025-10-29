594c8b12f23af5eb7d018f1fb30619a4f7e01c6f
jooky <jooky0327@naver.com>
2025-10-29 11:03:31 +0900

    홈 변경

--- src/main/java/com/back/HomeController.java
+++ src/main/java/com/back/HomeController.java
@@ -5,14 +5,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequiredArgsConstructor
 public class HomeController {
 
     @GetMapping("/")
     public String home() {
         return """
-                <h1>Welcome to the Home Page of JooKwonYoung</h1>
-                                <p>This is a Demo application.</p>
+                <h1>www.jooky.site</h1>
+                                <p>spring boot 프로젝트</p>
+                                <p>자동배포 환경</p>
                 """;
     }
 }
